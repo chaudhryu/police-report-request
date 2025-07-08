01bd706fec5d8aa367c3c7d7f789ee0cb1c0a475
chaudhryu <135050470+chaudhryu@users.noreply.github.com>
2025-07-08 16:14:40 -0700

    Adding files for login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense, useEffect } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes, BrowserRouter } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { MsalAuthenticationTemplate, UnauthenticatedTemplate } from '@azure/msal-react'
 
 import { CSpinner, useColorModes } from '@coreui/react'
 import './scss/style.scss'
@@ -36,23 +37,27 @@ const App = () => {
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <HashRouter>
-      <Suspense
-        fallback={
-          <div className="pt-3 text-center">
-            <CSpinner color="primary" variant="grow" />
-          </div>
-        }
-      >
-        <Routes>
-          <Route exact path="/login" name="Login Page" element={<Login />} />
-          <Route exact path="/register" name="Register Page" element={<Register />} />
-          <Route exact path="/404" name="Page 404" element={<Page404 />} />
-          <Route exact path="/500" name="Page 500" element={<Page500 />} />
-          <Route path="*" name="Home" element={<DefaultLayout />} />
-        </Routes>
-      </Suspense>
-    </HashRouter>
+    <>
+      <MsalAuthenticationTemplate>
+        <BrowserRouter>
+          <DefaultLayout />
+        </BrowserRouter>
+      </MsalAuthenticationTemplate>
+
+      <UnauthenticatedTemplate>
+        <BrowserRouter>
+          <Suspense fallback={<CSpinner />}>
+            <Routes>
+              <Route exact path="/login" name="Login Page" element={<Login />} />
+              <Route exact path="/register" name="Register Page" element={<Register />} />
+              <Route exact path="/404" name="Page 404" element={<Page404 />} />
+              <Route exact path="/500" name="Page 500" element={<Page500 />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </UnauthenticatedTemplate>
+    </>
+
   )
 }
 
