ca713c4e3cf3d58487655f56a3853ae15665579a
chaudhryu <135050470+chaudhryu@users.noreply.github.com>
2025-07-17 14:00:24 -0700

    Lots of clean up and changes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,70 @@
-import React, { Suspense, useEffect } from 'react'
-import { HashRouter, Route, Routes, BrowserRouter } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { MsalAuthenticationTemplate, UnauthenticatedTemplate } from '@azure/msal-react'
+// src/App.js
+import React, { Suspense, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {
+  MsalAuthenticationTemplate,
+  UnauthenticatedTemplate,
+} from '@azure/msal-react';
+import { InteractionType } from '@azure/msal-browser';
+import { loginRequest } from './config/authConfig';
+import { CSpinner, useColorModes } from '@coreui/react';
 
-import { CSpinner, useColorModes } from '@coreui/react'
-import './scss/style.scss'
+import './scss/style.scss';
+import './scss/examples.scss';
 
-// We use those styles to show code examples, you should remove them in your application.
-import './scss/examples.scss'
+// Lazy-loaded containers / pages
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
+const Login       = React.lazy(() => import('./views/pages/login/Login'));
+const Register    = React.lazy(() => import('./views/pages/register/Register'));
+const Page404     = React.lazy(() => import('./views/pages/page404/Page404'));
+const Page500     = React.lazy(() => import('./views/pages/page500/Page500'));
 
-// Containers
-const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
-
-// Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+// Small wrapper so MsalAuthenticationTemplate receives a *component*, not an element
+const Loader = () => <CSpinner color="primary" />;
 
 const App = () => {
-  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
-  const storedTheme = useSelector((state) => state.theme)
+  const { isColorModeSet, setColorMode } = useColorModes(
+    'coreui-free-react-admin-template-theme',
+  );
+  const storedTheme = useSelector((state) => state.theme);
 
+  // keep your colour-mode side-effect as-is
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
-    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
-    if (theme) {
-      setColorMode(theme)
-    }
+    const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
+    const theme =
+      urlParams.get('theme')?.match(/^[A-Za-z0-9\s]+/)?.[0] ?? null;
 
-    if (isColorModeSet()) {
-      return
-    }
-
-    setColorMode(storedTheme)
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+    if (theme) setColorMode(theme);
+    else if (!isColorModeSet()) setColorMode(storedTheme);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <>
-      <MsalAuthenticationTemplate>
-        <BrowserRouter>
+    <BrowserRouter>
+      {/* -------- Signed-in branch -------- */}
+      <MsalAuthenticationTemplate
+        interactionType={InteractionType.Redirect}
+        authenticationRequest={loginRequest}
+        loadingComponent={Loader}     // <- component *reference*, not <JSX/>
+      >
+        <Suspense fallback={<Loader />}>
           <DefaultLayout />
-        </BrowserRouter>
+        </Suspense>
       </MsalAuthenticationTemplate>
 
+      {/* -------- Signed-out branch -------- */}
       <UnauthenticatedTemplate>
-        <BrowserRouter>
-          <Suspense fallback={<CSpinner />}>
-            <Routes>
-              <Route exact path="/login" name="Login Page" element={<Login />} />
-              <Route exact path="/register" name="Register Page" element={<Register />} />
-              <Route exact path="/404" name="Page 404" element={<Page404 />} />
-              <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/login"    element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/404"     element={<Page404 />} />
+            <Route path="/500"     element={<Page500 />} />
+          </Routes>
+        </Suspense>
       </UnauthenticatedTemplate>
-    </>
+    </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import 'core-js'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
 
-import App from './App'
-import store from './store'
+import App from "./App";
+import store from "./store";
+import { msalConfig } from "./config/authConfig";   // one “./”, not “../src/”
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+const msalInstance = new PublicClientApplication(msalConfig);
+
+createRoot(document.getElementById("root")).render(
+  <MsalProvider instance={msalInstance}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MsalProvider>
+);
